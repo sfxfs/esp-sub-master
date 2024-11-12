78b937256bd0ab627f1ab6d57901e506c803d6e6
sfxfs <2501683578@qq.com>
2024-11-12 14:48:28 +0800

    Add SPI2/SPI3 config and init, update default flash size

--- components/sub_bus_init/sub_spi.c
+++ components/sub_bus_init/sub_spi.c
@@ -6,11 +6,43 @@
 esp_err_t sub_spi_intf_init(void)
 {
     esp_err_t ret = ESP_OK;
+#if CONFIG_SUB_ENABLE_SPI2
+{
+    spi_bus_config_t buscfg = {
+        .miso_io_num = CONFIG_SUB_SPI2_MISO_PIN,
+        .mosi_io_num = CONFIG_SUB_SPI2_MOSI_PIN,
+        .sclk_io_num = CONFIG_SUB_SPI2_CLK_PIN,
+        .quadwp_io_num = -1,
+        .quadhd_io_num = -1,
+        .max_transfer_sz = SOC_SPI_MAXIMUM_BUFFER_SIZE,
+    };
+    ret += spi_bus_initialize(SPI2_HOST, &buscfg, SPI_DMA_CH_AUTO);
+}
+#endif
+#if CONFIG_SUB_ENABLE_SPI3
+{
+    spi_bus_config_t buscfg = {
+        .miso_io_num = CONFIG_SUB_SPI3_MISO_PIN,
+        .mosi_io_num = CONFIG_SUB_SPI3_MOSI_PIN,
+        .sclk_io_num = CONFIG_SUB_SPI3_CLK_PIN,
+        .quadwp_io_num = -1,
+        .quadhd_io_num = -1,
+        .max_transfer_sz = SOC_SPI_MAXIMUM_BUFFER_SIZE,
+    };
+    ret += spi_bus_initialize(SPI3_HOST, &buscfg, SPI_DMA_CH_AUTO);
+}
+#endif
     return ret;
 }
 
 esp_err_t sub_spi_intf_deinit(void)
 {
     esp_err_t ret = ESP_OK;
+#if CONFIG_SUB_ENABLE_SPI2
+    ret += spi_bus_free(SPI2_HOST);
+#endif
+#if CONFIG_SUB_ENABLE_SPI3
+    ret += spi_bus_free(SPI3_HOST);
+#endif
     return ret;
 }
