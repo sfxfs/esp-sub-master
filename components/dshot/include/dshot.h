a800839de2fa372de77480e2c2d3b73c9ce536d3
sfxfs <2501683578@qq.com>
2024-12-18 17:45:15 +0800

    Update DShot dependency and main.c includes

--- components/dshot/include/dshot.h
+++ components/dshot/include/dshot.h
@@ -7,6 +7,7 @@
 
 #include <stdint.h>
 #include <stdbool.h>
+
 #include "driver/rmt_encoder.h"
 
 #ifdef __cplusplus
--- main/main.c
+++ main/main.c
@@ -1,13 +1,20 @@
+// stdlibs
 #include <stdio.h>
 
+// offical components
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
 #include "esp_log.h"
 
-#include "pb_com.h"
+// components
+#include "dshot.h"
 #include "aht30_app.h"
 #include "pca9685_app.h"
 #include "sub_intf_init.h"
+
+// mains
+#include "pb_com.h"
+#include "navi_master.pb.h"
 
 void app_main(void)
 {
