dc5c11d3c5958aa6973c98a3944369f474012581
sfxfs <2501683578@qq.com>
2024-12-21 21:00:26 +0800

    Refactor sub_bus_init, remove sub_i2c/sub_spi, update components

--- components/sub_bus_init/include/sub_spi.h
+++
@@ -1,9 +0,0 @@
-#ifndef SUB_INTF_INIT_SPI_H
-#define SUB_INTF_INIT_SPI_H
-
-#include "esp_err.h"
-
-esp_err_t sub_spi_intf_init(void);
-esp_err_t sub_spi_intf_deinit(void);
-
-#endif
--- components/sub_bus_init/sub_intf_init.c
+++ components/sub_bus_init/sub_intf_init.c
@@ -1,20 +1,67 @@
-#include "sub_i2c.h"
-#include "sub_spi.h"
+#include "driver/spi_master.h"
+#include "driver/i2c_master.h"
+
+#include "sdkconfig.h"
 
 #include "sub_intf_init.h"
 
-esp_err_t sub_all_intf_init(void)
+int sub_all_intf_init(void)
 {
-    esp_err_t ret = ESP_OK;
-    ret += sub_i2c_intf_init();
-    ret += sub_spi_intf_init();
-    return ret;
+    int ret = ESP_OK;
+// IIC ------------------------
+#if CONFIG_SUB_ENABLE_I2C0
+{
+    i2c_master_bus_handle_t i2c0_bus_handle;
+    i2c_master_bus_config_t i2c_bus_config = {
+        .i2c_port = 0,
+        .sda_io_num = CONFIG_SUB_I2C0_SDA_PIN,
+        .scl_io_num = CONFIG_SUB_I2C0_SCL_PIN,
+        .clk_source = I2C_CLK_SRC_DEFAULT,
+        .glitch_ignore_cnt = 7,
+    };
+    ret += i2c_new_master_bus(&i2c_bus_config, &i2c0_bus_handle);
 }
-
-esp_err_t sub_all_intf_deinit(void)
+#endif
+#if CONFIG_SUB_ENABLE_I2C1
 {
-    esp_err_t ret = ESP_OK;
-    ret += sub_i2c_intf_deinit();
-    ret += sub_spi_intf_deinit();
+    i2c_master_bus_handle_t i2c1_bus_handle;
+    i2c_master_bus_config_t i2c_bus_config = {
+        .i2c_port = 1,
+        .sda_io_num = CONFIG_SUB_I2C1_SDA_PIN,
+        .scl_io_num = CONFIG_SUB_I2C1_SCL_PIN,
+        .clk_source = I2C_CLK_SRC_DEFAULT,
+        .glitch_ignore_cnt = 7,
+    };
+    ret += i2c_new_master_bus(&i2c_bus_config, &i2c1_bus_handle);
+}
+#endif
+
+// SPI ------------------------
+#if CONFIG_SUB_ENABLE_SPI2
+{
+    spi_bus_config_t buscfg = {
+        .miso_io_num = CONFIG_SUB_SPI2_MISO_PIN,
+        .mosi_io_num = CONFIG_SUB_SPI2_MOSI_PIN,
+        .sclk_io_num = CONFIG_SUB_SPI2_CLK_PIN,
+        .quadwp_io_num = -1,
+        .quadhd_io_num = -1,
+        .max_transfer_sz = SOC_SPI_MAXIMUM_BUFFER_SIZE,
+    };
+    ret += spi_bus_initialize(SPI2_HOST, &buscfg, SPI_DMA_CH_AUTO);
+}
+#endif
+#if CONFIG_SUB_ENABLE_SPI3
+{
+    spi_bus_config_t buscfg = {
+        .miso_io_num = CONFIG_SUB_SPI3_MISO_PIN,
+        .mosi_io_num = CONFIG_SUB_SPI3_MOSI_PIN,
+        .sclk_io_num = CONFIG_SUB_SPI3_CLK_PIN,
+        .quadwp_io_num = -1,
+        .quadhd_io_num = -1,
+        .max_transfer_sz = SOC_SPI_MAXIMUM_BUFFER_SIZE,
+    };
+    ret += spi_bus_initialize(SPI3_HOST, &buscfg, SPI_DMA_CH_AUTO);
+}
+#endif
     return ret;
 }
