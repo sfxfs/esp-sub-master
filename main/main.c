709b545e19aff41b579174391d2e6cd3eff35d39
sfxfs <2501683578@qq.com>
2024-12-22 10:54:48 +0800

    Add protobuf communication and init in main.c

--- main/main.c
+++ main/main.c
@@ -8,12 +8,58 @@
 
 
 // components
-#include "dshot.h"
-#include "aht30_app.h"
-#include "pca9685_app.h"
+#include "message_cmd.h"
+#include "pb_rpc.h"
 #include "sub_intf_init.h"
+
+static const char *TAG = "main";
 
 void app_main(void)
 {
+    // 各总线端口初始化 spi iic
+    if (0 != sub_all_intf_init())
+    {
+        for (;;)
+        {
+            ESP_LOGE(TAG, "sub_all_intf_init failed");
+            vTaskDelay(1000 / portTICK_PERIOD_MS);
+        }
+    }
 
+    // protobuf通信用到的外设初始化
+    if (0 != message_cmd_init())
+    {
+        for (;;)
+        {
+            ESP_LOGE(TAG, "message_cmd_init failed");
+            vTaskDelay(1000 / portTICK_PERIOD_MS);
+        }
+    }
+
+    // protobuf通信初始化
+    if (0 != protobuf_commu_init())
+    {
+        for (;;)
+        {
+            ESP_LOGE(TAG, "protobuf_commu_init failed");
+            vTaskDelay(1000 / portTICK_PERIOD_MS);
+        }
+    }
+
+    // protobuf通信线程启动
+    if (0 != protobuf_commu_start_thread())
+    {
+        for (;;)
+        {
+            ESP_LOGE(TAG, "protobuf_commu_start_thread failed");
+            vTaskDelay(1000 / portTICK_PERIOD_MS);
+        }
+    }
+
+    // 无限循环，发送心跳包
+    for (;;)
+    {
+        vTaskDelay(1000 / portTICK_PERIOD_MS);
+        // heartbeat packet TODO ...
+    }
 }
