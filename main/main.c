5de2623b7a3dddb505c1e57385db68ac02dca41f
sfxfs <2501683578@qq.com>
2025-09-04 23:57:52 +0800

    Delete redundant files

--- main/main.c
+++ main/main.c
@@ -6,74 +6,7 @@
 #include "freertos/task.h"
 #include "esp_log.h"
 
-#include "jy901.h"
-
-// main
-#include "sub_rpc.h"
-#include "sub_rpc_func.h"
-#include "sub_intf_init.h"
-
-static const char *TAG = "main";
-
 void app_main(void)
 {
-    // 各总线端口初始化 spi iic
-    if (ESP_OK != sub_bus_intf_init())
-    {
-        for (;;)
-        {
-            ESP_LOGE(TAG, "sub_all_intf_init failed");
-            vTaskDelay(1000 / portTICK_PERIOD_MS);
-        }
-    }
-    ESP_LOGI(TAG, "sub_all_intf_init successed");
 
-    // protobuf通信用到的外设初始化
-    if (ESP_OK != sub_rpc_handle_func_init())
-    {
-        for (;;)
-        {
-            ESP_LOGE(TAG, "sub_rpc_handle_func_init failed");
-            vTaskDelay(1000 / portTICK_PERIOD_MS);
-        }
-    }
-    ESP_LOGI(TAG, "sub_rpc_handle_func_init successed");
-
-    // protobuf通信初始化
-    if (ESP_OK != sub_rpc_init())
-    {
-        for (;;)
-        {
-            ESP_LOGE(TAG, "sub_rpc_init failed");
-            vTaskDelay(1000 / portTICK_PERIOD_MS);
-        }
-    }
-    ESP_LOGI(TAG, "sub_rpc_init successed");
-
-    // JY901初始化
-    if (ESP_OK == jy901_init())
-        if (ESP_OK == jy901_routine_start())
-            ESP_LOGI(TAG, "jy901_routine_start success");
-        else
-            ESP_LOGE(TAG, "jy901_routine_start fail");
-    else
-        ESP_LOGE(TAG, "jy901_init fail");
-
-    // protobuf通信线程启动
-    if (ESP_OK != sub_rpc_start_thread())
-    {
-        for (;;)
-        {
-            ESP_LOGE(TAG, "sub_rpc_start_thread failed");
-            vTaskDelay(1000 / portTICK_PERIOD_MS);
-        }
-    }
-    ESP_LOGI(TAG, "sub_rpc_start_thread successed");
-
-    // 无限循环，发送心跳包
-    for (;;)
-    {
-        vTaskDelay(1000 / portTICK_PERIOD_MS);
-        // heartbeat packet TODO ...
-    }
 }
